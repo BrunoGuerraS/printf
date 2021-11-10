90ba638e78cd217a324bcfc7baa54deb728c0594
mr.b <bruno932151@gmail.com>
2021-11-09 21:31:43 -0500

    up date

--- get_type_data.c
+++ get_type_data.c
@@ -1,22 +1,25 @@
 #include "main.h"
-
+/**
+ * get_type_data - genereited mach
+ * @t: char
+ * Return: pinter to function
+ */
 int (*get_type_data(char t))(va_list, char **buff)
 {
-    int i = 0;
-    tp_d tp[] = {
-        {'s', _string},
-        {'c', _char},
-        {0, NULL}
-    };
+	int i = 0;
+	tp_d tp[] = {
+		{'s', _string},
+		{'c', _char},
+		{0, NULL}
+	};
 
-    while (tp[i].f != NULL)
-    {
-        if (t == tp[i].td)
-        {
-            //estoy retornando un puntero que apunta a esa funcion 
-            return (tp[i].f);
-        }
-        i++;
-    }
-    return (0);
+	while (tp[i].f != NULL)
+	{
+		if (t == tp[i].td)
+		{
+			return (tp[i].f);
+		}
+		i++;
+	}
+	return (0);
 }
