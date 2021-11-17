3d72b6ddbc2ba725bb5bef051e950a471a39a24e
mr.b <bruno932151@gmail.com>
2021-11-16 22:39:32 -0500

    task 1 was up date

--- _handelId.c
+++ _handelId.c
@@ -5,11 +5,36 @@
  * @buff: buffer
  * Return: handel function
  */
-int _handelId(va_list tp, char **buff)
-{
-	char dato = va_arg(tp, int);
-	**buff = dato;
-	(*buff)++;
 
+int print_number(long int n, char **add)
+{
+    unsigned int r;
+    int _length = 0;
+
+    if (n < 0)
+    {
+        **add = 45;
+        (*add)++;
+        /*_putchar(45);*/
+        n *= -1;
+        _length++;
+    }
+    r = n;
+
+    if (r / 10)
+    {
+        _length += print_number(r / 10, add);
+    }
+    **add = r % 10 + '0';
+    (*add)++;
+    /*_putchar(r % 10 + '0');*/
+    return (0);
+}
+
+int _handelId(va_list tp, char **buff)
+{	
+	int dato = va_arg(tp, int);
+
+	print_number(dato, buff);
 	return (0);
 }
