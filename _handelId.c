ff78e91ce43dc9773e88661a27ef103f24f30c58
mr.b <bruno932151@gmail.com>
2021-11-16 22:48:29 -0500

    passed test betty

--- _handelId.c
+++ _handelId.c
@@ -1,38 +1,40 @@
 #include "main.h"
 /**
+ * print_number - select a number
+ * @n: long int
+ * @add: double pointer
+ * Return: 0
+ */
+int print_number(long int n, char **add)
+{
+	unsigned int r;
+	int _length = 0;
+
+	if (n < 0)
+	{
+		**add = 45;
+		(*add)++;
+		n *= -1;
+		_length++;
+	}
+	r = n;
+
+	if (r / 10)
+	{
+		_length += print_number(r / 10, add);
+	}
+	**add = r % 10 + '0';
+	(*add)++;
+	return (0);
+}
+/**
  * _handelId - handle int and d
  * @tp: va_list
  * @buff: buffer
  * Return: handel function
  */
-
-int print_number(long int n, char **add)
-{
-    unsigned int r;
-    int _length = 0;
-
-    if (n < 0)
-    {
-        **add = 45;
-        (*add)++;
-        /*_putchar(45);*/
-        n *= -1;
-        _length++;
-    }
-    r = n;
-
-    if (r / 10)
-    {
-        _length += print_number(r / 10, add);
-    }
-    **add = r % 10 + '0';
-    (*add)++;
-    /*_putchar(r % 10 + '0');*/
-    return (0);
-}
-
 int _handelId(va_list tp, char **buff)
-{	
+{
 	int dato = va_arg(tp, int);
 
 	print_number(dato, buff);
